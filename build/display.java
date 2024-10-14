83c553b628bffe986460cd8b2ddb0283551b6dd3
benaoudia <145931083+benaoudia@users.noreply.github.com>
2024-10-14 20:42:46 +0100

    Add files via upload

---
+++ build/display.java
@@ -0,0 +1,10 @@
+public class display {
+    private static void display(String message) {
+        System.out.println(message);
+    }
+               
+    // public static void main(String[] args){
+    //     String msg = "hello ";
+    //     display(msg);
+    // }
+}
